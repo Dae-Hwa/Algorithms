349fa0f5481283cb9cc67ae98370754de1ebb614
Dae-Hwa <daehwa_jeong@outlook.com>
2019-10-18 19:56:29 +0900

    dfs구현

--- DFS/src/baekjoon/q1260/Main.java
+++ DFS/src/baekjoon/q1260/Main.java
@@ -1,5 +1,6 @@
 package baekjoon.q1260;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -28,32 +29,66 @@ public class Main {
 		graph.setAdjacent(1, 4);
 		graph.setAdjacent(2, 4);
 		graph.setAdjacent(3, 4);
-
-		System.out.println(Arrays.deepToString(graph.graph));
+		graph.dfs(1);
+		
+		System.out.println(graph.getResult());
 	}
 }
 
 class Graph {
-	LinkedList<Vertax>[] graph;
+	private LinkedList<Integer>[] graph;
+	private LinkedList<Integer> stack = new LinkedList<>();
+	private boolean[] walked;
+	private ArrayList<Integer> result = new ArrayList<>();
 
 	public Graph(int n) {
 		graph = new LinkedList[n];
-
+		walked = new boolean[n];
 		for (int i = 0; i < n; i++) {
-			graph[i] = new LinkedList<Vertax>();
+			graph[i] = new LinkedList<Integer>();
 		}
 
 	}
 
 	public void setAdjacent(int vertax, int adjacent) {
-		graph[vertax-1].add(new Vertax(adjacent, false));
+		graph[vertax - 1].add(adjacent);
 	}
 
 	@Override
 	public String toString() {
 		return "Graph [graph=" + Arrays.toString(graph) + "]";
 	}
+	public ArrayList<Integer> dfs(int index) {
+		result.add(index);
+		executeDfs(index);
+		return result;
+	}
+	public void executeDfs(int index) {
+		if (!graph[index-1].isEmpty()) {
+			pushAdjacent(stack, graph[index-1]);
+		}
+		System.out.println(stack);
+		
+		if (!stack.isEmpty()) {
+			int vertax = stack.pop();
+			if (!walked[vertax-1]) {
+				walked[vertax-1]=true;
+				result.add(vertax);
+				executeDfs(vertax);
+			}
+		}
+	}
 
+	private void pushAdjacent(LinkedList<Integer> stack, LinkedList<Integer> adjacent) {
+		
+		for (int i = adjacent.size()-1; i >= 0; i--) {
+			stack.push(adjacent.get(i));
+		}
+	}
+	
+	public ArrayList<Integer> getResult(){
+		return result;
+	}
 }
 
 class Vertax {
