a5b4bc33db609f2764b195640d80ef44a17bc3ff
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-03-15 21:18:34 +0900

    병합정렬(반복문)   - 결과 배열에 끼워넣는 방식으로 해야함 -> 가장 아랫부분 완료  감싸는 반복문 해결 필요

--- src/bj2751/Main.java
+++ src/bj2751/Main.java
@@ -5,37 +5,48 @@ import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     int endPoint = 5;
-    int[] input = { 5, 4, 3, 2, 1 };
-    int[] result = new int[input.length];
+    int[] input = { 3, 5, 2, 4, 1 };// { 5, 4, 3, 2, 1 };
+    int[] result;
     int left = 0;
     int leftEnd = 0;
+    int right = 0;
     int rightEnd = 0;
     int temp = 0;
+    int pointer = 0;
 
-    for (int mergeSize = 1; mergeSize < endPoint; mergeSize <<= 1) {
+    // l과 r 비교해서 병합
+    left = 0;
+    leftEnd = 1;
+    right = 2;
+    rightEnd = 3;
+    result = Arrays.copyOf(input, input.length);
 
-      for (int right = mergeSize; right < endPoint; right += mergeSize << 1) {
-        left = right - mergeSize;
-        rightEnd = right + mergeSize;
-        leftEnd = right - 1;
-        do {
-          if (input[right] < input[left]) {
-            temp = input[right];
-            input[right] = input[left];
-            input[left] = temp;
+    for (pointer = left; pointer <= rightEnd; pointer++) {
 
-            if (right < rightEnd - 1 && right < endPoint)
-              right++;
-
-          } else {
-            if (left < leftEnd)
-              left++;
-
-          }
-        } while (left < leftEnd && right < rightEnd && !(endPoint <= right));
+      if (right <= rightEnd && input[right] <= input[left]) {
+        result[pointer] = input[right];
+        right++;
+      } else if (left <= leftEnd && input[left] <= input[right]) {
+        result[pointer] = input[left];
+        left++;
+      } else if (rightEnd < right) {
+        for (; left <= leftEnd; left++) {
+          result[pointer] = input[left];
+          pointer++;
+        }
+        break;
+      } else if (leftEnd < left) {
+        for (; right <= rightEnd; right++) {
+          result[pointer] = input[right];
+          pointer++;
+        }
+        break;
       }
-
     }
+
+    input = result;
+
     System.out.println(Arrays.toString(result));
+
   }
 }
