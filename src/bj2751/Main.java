fccb4c9877114c3ddd7c0bd29fa580ad87093e99
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-03-18 03:58:48 +0900

    병합정렬(for문) - 반복문과 로직 합침   -err :  mergeSize 4 일때, endPoint에 막혀서 동작하지 않음

--- src/bj2751/Main.java
+++ src/bj2751/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
-    int endPoint = 5;
+    int endPoint = 5 - 1;
     int[] input = { 3, 5, 2, 4, 1 };// { 5, 4, 3, 2, 1 };
     int[] result;
     int left = 0;
@@ -47,7 +47,8 @@ class Main {
     }
     input = result;
 
-    System.out.println(Arrays.toString(result));
+    // System.out.println(Arrays.toString(result));
+
     // 바깥쪽 for문
     /**
      * the result what I want is
@@ -95,7 +96,37 @@ class Main {
         // System.out.println("leftEnd" + leftEnd);
         // System.out.println("right" + right);
         // System.out.println("rightEnd" + rightEnd);
+        result = Arrays.copyOf(input, input.length);
+
+        for (pointer = left; pointer <= endPoint; pointer++) {
+
+          if (right <= rightEnd && right <= endPoint && input[right] <= input[left]) {
+            result[pointer] = input[right];
+            right++;
+          } else if (left <= leftEnd && right <= endPoint && input[left] <= input[right]) {
+            result[pointer] = input[left];
+            left++;
+          } else if (rightEnd < right) {
+            for (; left <= leftEnd; left++) {
+              result[pointer] = input[left];
+              pointer++;
+            }
+
+            break;
+          } else if (leftEnd < left) {
+            for (; right <= rightEnd; right++) {
+              result[pointer] = input[right];
+              pointer++;
+            }
+
+            break;
+          }
+        }
+        left--;
+        right--;
+        input = result;
       }
     }
+    System.out.println(Arrays.toString(result));
   }
 }
