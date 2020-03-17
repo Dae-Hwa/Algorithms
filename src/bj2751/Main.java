147697e5a6d0dd85d19f358e5c9e1e4751cc9ac4
Dae-Hwa <daehwa_jeong@outlook.com>
2020-03-17 19:48:17 +0900

    병합정렬 - 바깥쪽 for 구문   - 예상 출력값 작성

--- src/bj2751/Main.java
+++ src/bj2751/Main.java
@@ -4,38 +4,42 @@ import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
-    int endPoint = 5;
+    int endPoint = 5-1;
     int[] input = { 5, 4, 3, 2, 1 };
-    int[] result = new int[input.length];
+    int[] result = Arrays.copyOf(input,input.length);
     int left = 0;
     int leftEnd = 0;
+    int right = 0;
     int rightEnd = 0;
+    int mergeSize = 0;
     int temp = 0;
 
-    for (int mergeSize = 1; mergeSize < endPoint; mergeSize <<= 1) {
+    /**
+     * the result what I want is
+     * 
+     * 1. ms=1
+     * 
+     * R 0 2 4 
+     * 
+     * L 1 3 4 // endpoint-1
+     * 
+     * 2. ms=2
+     * 
+     * R 0 4
+     * 
+     * L 2 4 // endpoint-1
+     * 
+     * 3. ms=4
+     * 
+     * R 0123 8,9,1011
+     *  
+     * L 4567 12131415
+     * 
+     * //actual
+     * 
+     * R 0123
+     *  
+     * L 4 //endpoint 
+     */
 
-      for (int right = mergeSize; right < endPoint; right += mergeSize << 1) {
-        left = right - mergeSize;
-        rightEnd = right + mergeSize;
-        leftEnd = right - 1;
-        do {
-          if (input[right] < input[left]) {
-            temp = input[right];
-            input[right] = input[left];
-            input[left] = temp;
-
-            if (right < rightEnd - 1 && right < endPoint)
-              right++;
-
-          } else {
-            if (left < leftEnd)
-              left++;
-
-          }
-        } while (left < leftEnd && right < rightEnd && !(endPoint <= right));
-      }
-
-    }
-    System.out.println(Arrays.toString(result));
-  }
 }
