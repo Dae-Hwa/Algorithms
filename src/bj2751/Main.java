6ea4f469281fc1b248f503a55ec119786451298c
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-03-18 03:21:17 +0900

    병합정렬(for문) - 바깥쪽 for문 작성   - 인덱스 초과될 경우 확인해야함

--- src/bj2751/Main.java
+++ src/bj2751/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) {
-    int endPoint = 5-1;
+    int endPoint = 16 - 1;
     int[] input = { 5, 4, 3, 2, 1 };
-    int[] result = Arrays.copyOf(input,input.length);
+    int[] result = Arrays.copyOf(input, input.length);
     int left = 0;
     int leftEnd = 0;
     int right = 0;
@@ -19,27 +19,50 @@ class Main {
      * 
      * 1. ms=1
      * 
-     * R 0 2 4 
+     * L 0 2 4
      * 
-     * L 1 3 4 // endpoint-1
+     * R 1 3 4 // endpoint-1
      * 
      * 2. ms=2
      * 
-     * R 0 4
+     * L 0 4
      * 
-     * L 2 4 // endpoint-1
+     * R 2 4 // endpoint-1
      * 
      * 3. ms=4
      * 
-     * R 0123 8,9,1011
-     *  
-     * L 4567 12131415
+     * L 0123 8,9,1011
+     * 
+     * R 4567 12131415
      * 
      * //actual
      * 
-     * R 0123
-     *  
-     * L 4 //endpoint 
+     * L 0123
+     * 
+     * R 4 //endpoint
+     * 
+     * r = r+me re = r+me-1 l = right-ms le = right-1
      */
+
+    for (int i = 1; i <= endPoint; i <<= 1) {
+      right = 0;
+      rightEnd = 0;
+      left = 0;
+      leftEnd = 0;
+      System.out.println("ms" + i);
+      for (; rightEnd < endPoint;) {
+        if (right != 0)
+          left = right + i;
+        right = left + i;
+        leftEnd = right - 1;
+        rightEnd = right + i - 1;
+        // System.out.println("left" + left);
+        // System.out.println("leftEnd" + leftEnd);
+        // System.out.println("right" + right);
+        // System.out.println("rightEnd" + rightEnd);
+      }
+
+    }
+  }
 
 }
