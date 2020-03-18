a23d625a6b62a78aadcc3602b6cb213318873489
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-03-19 00:45:40 +0900

    병합정렬(for문) - 완성   - 테스트 필요

--- src/bj2751/Main.java
+++ src/bj2751/Main.java
@@ -5,120 +5,62 @@ import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     int endPoint = 5 - 1;
-    int[] input = { 3, 5, 2, 4, 1 };// { 5, 4, 3, 2, 1 };
-    int[] result;
+    int[] input = { 4, 5, 3, 2, 1 };// { 5, 4, 3, 2, 1 };
+    int[] result = null;
     int left = 0;
     int leftEnd = 0;
     int right = 0;
     int rightEnd = 0;
     int mergeSize = 0;
-    int temp = 0;
     int pointer = 0;
 
-    // 안쪽 로직
-    // l과 r 비교해서 병합
-    left = 0;
-    leftEnd = 1;
-    right = 2;
-    rightEnd = 3;
-    result = Arrays.copyOf(input, input.length);
-
-    for (pointer = left; pointer <= rightEnd; pointer++) {
-
-      if (right <= rightEnd && input[right] <= input[left]) {
-        result[pointer] = input[right];
-        right++;
-      } else if (left <= leftEnd && input[left] <= input[right]) {
-        result[pointer] = input[left];
-        left++;
-      } else if (rightEnd < right) {
-        for (; left <= leftEnd; left++) {
-          result[pointer] = input[left];
-          pointer++;
-        }
-        break;
-      } else if (leftEnd < left) {
-        for (; right <= rightEnd; right++) {
-          result[pointer] = input[right];
-          pointer++;
-        }
-        break;
-      }
-    }
-    input = result;
-
-    // System.out.println(Arrays.toString(result));
-
-    // 바깥쪽 for문
-    /**
-     * the result what I want is
-     * 
-     * 1. ms=1
-     * 
-     * L 0 2 4
-     * 
-     * R 1 3 4 // endpoint-1
-     * 
-     * 2. ms=2
-     * 
-     * L 0 4
-     * 
-     * R 2 4 // endpoint-1
-     * 
-     * 3. ms=4
-     * 
-     * L 0123 8,9,1011
-     * 
-     * R 4567 12131415
-     * 
-     * //actual
-     * 
-     * L 0123
-     * 
-     * R 4 //endpoint
-     * 
-     * r = r+me re = r+me-1 l = right-ms le = right-1
-     */
-
     for (mergeSize = 1; mergeSize <= endPoint; mergeSize <<= 1) {
+      // 병합 크기가 변할때마다 0에서부터 시작
       right = 0;
       rightEnd = 0;
       left = 0;
       leftEnd = 0;
-      System.out.println("ms" + mergeSize);
+
       for (; rightEnd < endPoint;) {
         if (right != 0)
+          // 시작점에서 left가 0이어야함
           left = right + mergeSize;
         right = left + mergeSize;
         leftEnd = right - 1;
         rightEnd = right + mergeSize - 1;
-        // System.out.println("left" + left);
-        // System.out.println("leftEnd" + leftEnd);
-        // System.out.println("right" + right);
-        // System.out.println("rightEnd" + rightEnd);
+        // result에 deep copy 후 result에 결과병합(병합을 했을 때, 병합(정렬)되지 않은 부분은 그대로 있어야 함)
         result = Arrays.copyOf(input, input.length);
 
         for (pointer = left; pointer <= endPoint; pointer++) {
-
           if (right <= rightEnd && right <= endPoint && input[right] <= input[left]) {
+            // 오른쪽 부분이 결과에 채워지는 경우
             result[pointer] = input[right];
             right++;
           } else if (left <= leftEnd && right <= endPoint && input[left] <= input[right]) {
+            // 왼쪽 부분이 결과에 채워지는 경우
             result[pointer] = input[left];
             left++;
-          } else if (rightEnd < right) {
+          }
+          /**
+           * 왼쪽 혹은 오른쪽의 포인터가 범위를 벗어날 경우 반대쪽의 남은 요소들을 붙여주면 된다.
+           * 
+           * 남아있는 반대 쪽 요소들은 반드시 정렬된 상태이고,
+           * 
+           * 병합되지 않은 값들은 병합이 이루어진 값보다 항상 크다.
+           */
+          else if (rightEnd < right || endPoint < right) {
+            // right가 끝 지점에 다다랐을경우
             for (; left <= leftEnd; left++) {
               result[pointer] = input[left];
               pointer++;
             }
-
             break;
           } else if (leftEnd < left) {
+            // left가 right(경계)에 다다랐을경우
             for (; right <= rightEnd; right++) {
               result[pointer] = input[right];
               pointer++;
             }
-
             break;
           }
         }
