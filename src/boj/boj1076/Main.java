12d56c00016bab0737b26d19562acfa1e72971ec
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2021-01-06 12:02:01 +0900

    안풀림 6%에서 멈춤

---
+++ src/boj/boj1076/Main.java
@@ -0,0 +1,58 @@
+package boj.boj1076;
+
+import java.io.*;
+
+enum Color {
+    black(0, 1),
+    brown(1, 10),
+    red(2, 100),
+    orange(3, 1000),
+    yellow(4, 10000),
+    green(5, 100000),
+    blue(6, 1000000),
+    violet(7, 10000000),
+    grey(8, 100000000),
+    white(9, 1000000000);
+
+    final long VALUE;
+    final long MULTIPLE;
+
+    Color(long VALUE, int MULTIPLE) {
+        this.VALUE = VALUE;
+        this.MULTIPLE = MULTIPLE;
+    }
+
+    public static Color getColor(String colorName) {
+        for (Color color : values()) {
+            if (color.name().equals(colorName)) {
+                return color;
+            }
+        }
+
+        throw new IllegalArgumentException("없는 색깔");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] colors = new String[3];
+
+        colors[0] = br.readLine();
+        colors[1] = br.readLine();
+        colors[2] = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        long first = Color.getColor(colors[0]).VALUE;
+        long second = Color.getColor(colors[1]).VALUE * Color.getColor(colors[2]).MULTIPLE;
+
+        sb.append(first != 0 ? first : "")
+                .append(second != 0 ? second : "");
+
+        System.out.println(sb.length() == 0 ? 0 : sb);
+
+    }
+}
