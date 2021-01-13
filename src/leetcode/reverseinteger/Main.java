2c5319cce4abf912dfac44c29a9024cde4361ebb
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2021-01-13 11:51:55 +0900

    sovled with deque

---
+++ src/leetcode/reverseinteger/Main.java
@@ -0,0 +1,56 @@
+package leetcode.reverseinteger;
+
+
+import java.util.*;
+
+class Solution {
+    public int reverse(int x) {
+
+
+        Deque<String> deque = new ArrayDeque<>(Arrays.asList(String.valueOf(x).split("")));
+
+        StringBuilder sb = new StringBuilder();
+
+        int sign = 1;
+
+        if (x < 0) {
+            sign = -1;
+            deque.pollFirst();
+        }
+
+        while (!deque.isEmpty()) {
+            sb.append(deque.pollLast());
+        }
+        try {
+            return Integer.parseInt(sb.toString()) * sign;
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Object> inputs = new ArrayList<>(Arrays.asList(
+                new ArrayList(Arrays.asList(
+                        123, 321
+                )),
+                new ArrayList(Arrays.asList(
+                        -123, -321
+                )),
+                new ArrayList(Arrays.asList(
+                        1534236469, 0
+                ))
+
+        ));
+
+        for (int i = 0; i < inputs.size(); i++) {
+            List<Object> input = (List<Object>) inputs.get(i);
+
+            System.out.println("results");
+            System.out.println(new Solution().reverse((int) input.get(0)));
+            System.out.println("expected");
+            System.out.println(input.get(1));
+        }
+    }
+}
