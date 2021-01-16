eaa3f39a3d355a2693e9783c505a667f5ffc5d60
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2021-01-16 22:31:47 +0900

    failed

---
+++ src/boj/boj11656/Test.java
@@ -0,0 +1,28 @@
+package boj.boj11656;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1<<1);
+
+        List<Object> inputs = new ArrayList<>(Arrays.asList(
+//                "baekjoon"
+//                ,
+//                "banana"
+//                ,
+                "aaa"
+//                ,
+//                "abcabcabcabac"
+        ));
+
+        for (int i = 0; i < inputs.size(); i++) {
+//            List<Object> input = (List<Object>) inputs.get(i);
+
+            Main.solution((String) inputs.get(i));
+
+        }
+    }
+}
