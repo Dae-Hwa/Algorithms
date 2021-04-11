0c3d61baa53a6a8643391b8ba3ffea8b5c3c7688
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-04-11 11:54:42 +0900

    solved : 스트림 쓰지 않도록 첫 번째 풀이 수정

--- src/prgrms/단속카메라/단속카메라.java
+++ src/prgrms/단속카메라/단속카메라.java
@@ -1,27 +1,30 @@
 package prgrms.단속카메라;
 
 import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1;
+        int answer = 0;
 
         Arrays.sort(routes, Comparator.comparingInt(o -> o[0]));
 
-        int end = routes[0][1];
+        int nextExit = Integer.MAX_VALUE;
 
-        for (int i = 0; i < routes.length; i++) {
-            int enter = routes[i][0];
-            int exit = routes[i][1];
-
-            if (end < enter) {
-                answer += 1;
-                end = exit;
-            } else if (exit <= end) {
-                end = exit;
+        for (int[] enteredCar : routes) {
+            if (nextExit < enteredCar[0]) {
+                answer++;
+                nextExit = enteredCar[1];
+            } else if (nextExit == enteredCar[0]) {
+                answer++;
+                nextExit = Integer.MAX_VALUE;
+            } else if (enteredCar[1] <= nextExit) {
+                nextExit = enteredCar[1];
             }
+        }
+
+        if (nextExit != Integer.MAX_VALUE) {
+            answer++;
         }
 
         return answer;
