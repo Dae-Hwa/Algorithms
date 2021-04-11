26e3806f54e05a78024b510e6110c940755e2be7
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-04-11 11:44:58 +0900

    solved : 스트림 안 쓰니까 됨

--- src/prgrms/단속카메라/단속카메라.java
+++ src/prgrms/단속카메라/단속카메라.java
@@ -3,26 +3,24 @@ package prgrms.단속카메라;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
+        int answer = 1;
 
-        List<int[]> cars = Arrays.stream(routes)
-                .sorted(Comparator.comparing(ints -> ints[1]))
-                .collect(Collectors.toList());
+        Arrays.sort(routes, Comparator.comparingInt(o -> o[0]));
 
-        for (int i = 0; i < cars.size(); i++) {
-            answer++;
-            int exit = cars.get(i)[1];
+        int end = routes[0][1];
 
-            for (int j = i + 1; j < cars.size(); j++) {
-                if (cars.get(j)[0] <= exit) {
-                    cars.remove(j);
-                    j--;
-                }
+        for (int i = 0; i < routes.length; i++) {
+            int enter = routes[i][0];
+            int exit = routes[i][1];
+
+            if (end < enter) {
+                answer += 1;
+                end = exit;
+            } else if (exit <= end) {
+                end = exit;
             }
         }
 
@@ -33,6 +31,6 @@ class Solution {
 public class 단속카메라 {
     public static void main(String[] args) {
         PrintStream ps = System.out;
-        ps.println(new Solution().solution(new int[][]{{0, 1}, {0,1}, {1, 2}}));
+        ps.println(new Solution().solution(new int[][]{{0, 1}, {0, 1}, {1, 2}}));
     }
 }
